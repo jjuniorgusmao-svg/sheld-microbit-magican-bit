8b0da06f7901f4fec15b56fe965257d8f04d5e0d
jjuniorgusmao-svg <jjuniorgusmao@gmail.com>
2026-08-17 22:13:08 -0300

    Atualizar 'pxt.json, main.blocks, main.ts, SHIELD.ts'

--- SHIELD.ts
+++ SHIELD.ts
@@ -1,179 +1,77 @@
 /**
-
-* Use este arquivo para definir funções e blocos personalizados.
-
-* Saiba mais em https://makecode.microbit.org/blocks/custom
-
+* Definições de blocos para o Shield Magican Bit
 */
 
-
-
-enum MyEnum {
-
-  
-
-  
-         //% block="frente"
-
-    One,
-
-
-    
-        //% block="tras"
-
-    Two
-
-
-
+enum Sentido {
+    //% block="frente"
+    Frente = 0,
+    //% block="tras"
+    Tras = 1
 }
 
-enum MLouMR {
-
-
-     //% block="MR"
-    
-
-    One,
-
-  
+enum MotorSelect {
     //% block="ML"
-
-    Two
-
-
-
+    ML = 0,
+    //% block="MR"
+    MR = 1
 }
-
-
-
-/**
-
- * Custom blocks
-
- */
 
 //% weight=100 color=#FF6600 icon="\uf1b9"
-
 namespace SHIELD {
 
     /**
-
-     * TODO: describe your function here
-
-     * @param n describe parameter here, eg: 
-
-
-
-     * @param e describe parameter here
-
+     * Define a velocidade, sentido e motor
+     * @param n velocidade de 0 a 100, eg: 50
+     * @param e sentido do motor (frente ou tras)
+     * @param p motor a ser acionado (ML ou MR)
      */
-
-    //% block
-
-    export function velocidade(n: number, e: MyEnum, p: MLouMR): void {
-
+    //% block="velocidade %n sentido %e motor %p"
+    //% n.min=0 n.max=100 n.defl=50
+    //% e.defl=Sentido.Frente
+    //% p.defl=MotorSelect.ML
+    export function velocidade(n: number, e: Sentido, p: MotorSelect): void {
         let velocidade: number = 0
 
         if (n == 0) {
-
             velocidade = 0
-
+        } else {
+            velocidade = Math.map(n, 1, 100, 450, 1023)
         }
 
-        else {
-
-            velocidade = (Math.map(n, 0, 100, 450, 1023))
-
+        // Selecionou ML -> aciona os pinos P13 e P14 (MR original)
+        if (e == Sentido.Frente && p == MotorSelect.ML) {
+            pins.analogWritePin(AnalogPin.P13, velocidade)
+            pins.digitalWritePin(DigitalPin.P14, 0)
+        }
+        if (e == Sentido.Tras && p == MotorSelect.ML) {
+            pins.digitalWritePin(DigitalPin.P13, 0)
+            pins.analogWritePin(AnalogPin.P14, velocidade)
         }
 
-        if (e == MyEnum.One && p == MLouMR.One) {
-
+        // Selecionou MR -> aciona os pinos P15 e P16 (ML original)
+        if (e == Sentido.Frente && p == MotorSelect.MR) {
             pins.analogWritePin(AnalogPin.P15, velocidade)
             pins.digitalWritePin(DigitalPin.P16, 0)
-
         }
-
-        if (e == MyEnum.Two && p == MLouMR.One) {
-
+        if (e == Sentido.Tras && p == MotorSelect.MR) {
             pins.digitalWritePin(DigitalPin.P15, 0)
-
             pins.analogWritePin(AnalogPin.P16, velocidade)
-
-
-
         }
-
-        if (e == MyEnum.One && p == MLouMR.Two) {
-
-            pins.analogWritePin(AnalogPin.P13, velocidade)
-
-            pins.digitalWritePin(DigitalPin.P14, 0)
-
-        }
-
-        if (e == MyEnum.Two && p == MLouMR.Two) {
-
-            pins.digitalWritePin(DigitalPin.P13, 0)
-
-            pins.analogWritePin(AnalogPin.P14, velocidade)
-
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
-
-
-
-
 
     /**
-
-     * TODO: describe your function here
-
-     * @param value describe value here, eg: 5
-
+     * Para o motor selecionado
+     * @param P motor a ser desligado
      */
-
-    //% block
-
-    export function parar(P: MLouMR): void {
-
-        if (P == MLouMR.Two) {
-
-
-
+    //% block="parar motor %P"
+    //% P.defl=MotorSelect.ML
+    export function parar(P: MotorSelect): void {
+        if (P == MotorSelect.ML) {
             pins.digitalWritePin(DigitalPin.P13, 0)
-
             pins.digitalWritePin(DigitalPin.P14, 0)
-
         } else {
-
             pins.digitalWritePin(DigitalPin.P15, 0)
-
             pins.digitalWritePin(DigitalPin.P16, 0)
-
-
-
         }
-
     }
-
-
-
 }
-
-
-
--- main.ts
+++ main.ts
@@ -1,1 +1,3 @@
-
+SHIELD.velocidade(50, Sentido.Tras, MotorSelect.MR)
+basic.pause(100)
+SHIELD.parar(MotorSelect.MR)
